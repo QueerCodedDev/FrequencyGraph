e15eae8e7aff4bc5e8f14340efb65db04545206c
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-16 19:40:28 +0000

    Testing rendering the entire leverage line

--- classes/Line.js
+++ classes/Line.js
@@ -34,13 +34,16 @@ class Line {
     // The val_high should not be based on this.end, but based on the over-all range of dates.
     render() {
         stroke(this.color);
-        let x1 = map(new Date(this.start.date).getTime(), Settings.EARLIEST.getTime(), Settings.LATEST.getTime(), 0, Settings.canvasW);
-        let y1 = 1 * 10
-        let x2 = map(new Date(this.start.next.date).getTime(), Settings.EARLIEST.getTime(), Settings.LATEST.getTime(), 0, Settings.canvasW);
-        let y2 = 2 * 10;
+        for (let e of this.dataPoints) {
+            let x1 = map(new Date(this.start.date).getTime(), Settings.EARLIEST.getTime(), Settings.LATEST.getTime(), 0, Settings.canvasW);
+            let y1 = (this.dataPoints.indexOf(e) + 1) * 10;
+            let x2 = map(new Date(this.start.next.date).getTime(), Settings.EARLIEST.getTime(), Settings.LATEST.getTime(), 0, Settings.canvasW);
+            let y2 = (this.dataPoints.indexOf(e.next) + 1) * 10;
 
-        line(x1, y1, x2, y2);
+            line(x1, y1, x2, y2);
+            console.log(`X1: ${x1}\nY1: ${y1}\nX2: ${x2}\nY2: ${y2}\n`)
+        }
 
-        console.log(`X1: ${x1}\nY1: ${y1}\nX2: ${x2}\nY2: ${y2}\n`)
+        
     }
 }
