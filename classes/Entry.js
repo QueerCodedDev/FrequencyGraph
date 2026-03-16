264dc2da804e68d389e2350d4cb970ada041744d
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-16 20:27:23 +0000

    Never created dateOBJ, might be part of the sorting issue?

--- classes/Entry.js
+++ classes/Entry.js
@@ -4,6 +4,7 @@ class Entry {
         this.title = data.title;
         this.uni   = data.universe;
         this.date  = data.air_date;
+        this.dateOBJ = new Date(this.date);
 
         this.prev;
         this.next;
